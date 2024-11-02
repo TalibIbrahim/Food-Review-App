5913c09d7bc4a0ffb39700d2130553b7d09d198c
TalibIbrahim <112410392+TalibIbrahim@users.noreply.github.com>
2024-11-02 23:22:21 +0500

    styles for card

--- src/Components/RestaurantCard.js
+++ src/Components/RestaurantCard.js
@@ -1,26 +1,37 @@
-import ReactStars from 'react-stars'
-import React from 'react'
+import ReactStars from "react-stars";
+import React from "react";
+import "./RestaurantCard.css";
 
-const PriceSymbol={
-  1:"$",
-  2:"$$",
-  3:"$$$",
-}
-
+const PriceSymbol = {
+  1: "$",
+  2: "$$",
+  3: "$$$",
+};
 
 const RestaurantCard = (props) => {
   return (
-    <div className="flex flex-row w-full my-10 ">
+    <div className="flex flex-row w-full my-10 custom-card-bg shadow-lg p-10 rounded-2xl ">
       <div className="border border-neutral-200">
-        <img className="w-48 h-48 object-cover" src={props.logo} alt="restaurant logo" />
+        <img
+          className="w-48 h-48 object-cover"
+          src={props.logo}
+          alt="restaurant logo"
+        />
       </div>
       <div className="flex flex-col mx-5">
         <h1 className="text-3xl font-bold pb-1">{props.name}</h1>
         <p className="text-lg text-neutral-600">{props.location}</p>
-        
-          <ReactStars  count={5} size={24} value={props.rating} edit={false} half={true} color2={'#ffd700'} />
+
+        <ReactStars
+          count={5}
+          size={24}
+          value={props.rating}
+          edit={false}
+          half={true}
+          color2={"#ffd700"}
+        />
         <p>{props.cuisine}</p>
-        <p className='text-neutral-500'>{PriceSymbol[props.price]}</p>
+        <p className="text-neutral-500">{PriceSymbol[props.price]}</p>
       </div>
     </div>
   );
