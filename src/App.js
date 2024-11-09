103a7ff57b1c481168009fddb068e213efbb8401
muzammil-shayk <m.muzammil47@protonmail.com>
2024-11-09 20:15:40 +0500

    login page frontend design

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import "./App.css";
-import About from "./Pages/About";
-import LayoutWrapper from "./Layout/LayoutWrapper";
 import { Route, Routes } from "react-router-dom";
+import LayoutWrapper from "./Layout/LayoutWrapper";
+import Login from "./Pages/Login";
 import Home from "./Pages/Home";
+import About from "./Pages/About";
+
 function App() {
   return (
     <LayoutWrapper>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
       </Routes>
     </LayoutWrapper>
